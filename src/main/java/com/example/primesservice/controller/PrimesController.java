37e79560b54e2b297754e8098565de2382b877f2
tipb47 <tipb47@gmail.com>
2024-04-09 20:07:11 -0400

    p12

--- src/main/java/com/example/primesservice/controller/PrimesController.java
+++ src/main/java/com/example/primesservice/controller/PrimesController.java
@@ -1,20 +1,32 @@
 package com.example.primesservice.controller;
 
+import com.example.primesservice.rabbitmq.MQSender;
 import com.example.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/primes")
 public class PrimesController {
-   IPrimesService primesService;
-   public PrimesController(IPrimesService primesService)
+    private final MQSender mqSender;
+    IPrimesService primesService;
+   public PrimesController(IPrimesService primesService,
+                           MQSender mqSender)
    {
        this.primesService = primesService;
+       this.mqSender = mqSender;
    }
 
    @GetMapping("/{n}")
    public boolean isPrime(@PathVariable int n) {
-       return primesService.isPrime(n);
+       boolean result = primesService.isPrime(n);
+       Object principal = SecurityContextHolder
+               .getContext().getAuthentication().getPrincipal();
+       String username = ((Jwt) principal).getSubject();
+       System.out.println(username);
+       mqSender.sendMessage(username, n, result);
+       return result;
    }
 }
